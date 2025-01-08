ee9bddf309a80c89cfe4842713c3a83cc2be467a
leechiahwa <leechiahwa99@hotmail.com>
2025-01-08 13:23:49 +0800

    update connecting string to use render psql external url

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.HOSTNAME,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
